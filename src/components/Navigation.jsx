c4e8c27d0b3dad98ce30480b710f8e1e3fbbcec3
Tolga0342 <tolga_uslu@live.nl>
2024-09-25 02:20:11 +0200

    adding new page "AddEvent"

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navigation = () => {
   return (
@@ -10,6 +10,9 @@ export const Navigation = () => {
         </li>
         <li>
           <Link to="/event/1">Event</Link>
+        </li>
+        <li>
+          <Link to="/addevent"> add-event</Link>
         </li>
       </ul>
     </nav>
---
+++ src/pages/AddEvent.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+
+export const AddEvent = () => {
+  return <Heading> add event</Heading>;
+};
